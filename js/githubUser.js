10f053d230f27ba70b1f57bbd1f0be6b5e4af097
Ghimel Cândido <105327320+ghimelcandido@users.noreply.github.com>
2024-01-16 02:37:57 -0300

    Add files via upload

---
+++ js/githubUser.js
@@ -0,0 +1,15 @@
+export class githubUser {
+    static search(username) {
+        const url = `https://api.github.com/users/${username}`
+
+        return fetch(url)
+            .then(data => data.json())
+            .then(({ login, name, public_repos, followers }) => ({
+
+                login,
+                name,
+                public_repos,
+                followers
+            }))
+    }
+}
